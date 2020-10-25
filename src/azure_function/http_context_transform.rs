0451e7b771300c008e5783e77a6f9a6c61f6e357
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-10-24 21:51:55 -0700

    azure_function: clean up AzureFnMiddleware

--- src/azure_function/http_context_transform.rs
+++ src/azure_function/http_context_transform.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use async_std::sync::RwLock;
-use log::info;
 use serde_json::{json, Value};
 use tide::http::headers::CONTENT_TYPE;
 use tide::{Body, Middleware, Next, Request, Result};
@@ -34,36 +33,40 @@ impl AzureFnMiddleware {
         }
         req.set_ext(AzureFnMiddlewareHasBeenRun);
 
-        let req_data = req.body_string().await?;
-
-        info!("req_data: {}", req_data);
-
-        // let azure_function_payload: Value = req.body_json().await?;
-        let azure_function_payload: Value = serde_json::from_str(&req_data)?;
         let mut invocation_id = "(id missing)".to_string();
         if let Some(val) = req.header("X-Azure-Functions-InvocationId") {
             invocation_id = val.last().as_str().to_string();
         }
 
+        let mut logs = vec![];
+
+        let azure_function_payload: Value = req.body_json().await?;
+        if let Some(external_req_body) = azure_function_payload.pointer("/Data/req/Body") {
+            if let Value::String(body) = external_req_body {
+                req.set_body(Body::from_string(body.to_owned()));
+            } else {
+                logs.push(
+                    "AzureFnMiddleware Error: \"/Data/req/Body\" not a String, check function.json"
+                        .to_string(),
+                );
+            }
+        } else {
+            logs.push(
+                "AzureFnMiddleware Error: \"/Data/req/Body\" not found, check function.json"
+                    .to_string(),
+            );
+        }
+
         let logger = AzureFnLoggerInner {
-            logs: vec![],
+            logs,
             invocation_id,
         };
         let logger = Arc::new(RwLock::new(logger));
-
         req.set_ext(logger.clone());
-
-        if let Some(external_req_body) = azure_function_payload.pointer("/Data/req/Body") {
-            info!("external_req_body: {}", external_req_body);
-            if let Value::String(body) = external_req_body {
-                req.set_body(Body::from_string(body.to_owned()));
-            }
-        }
 
         let mut res = next.run(req).await;
 
         let logger = Arc::try_unwrap(logger).unwrap();
-
         let out = json!({
             "ReturnValue": res.take_body().into_string().await?,
             "Logs": logger.into_inner().logs,
