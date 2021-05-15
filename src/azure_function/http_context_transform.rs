2782be1d7f5d04ee7884e5cb90181fb522387960
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2021-05-15 14:11:41 -0700

    azure_function: map all headers to output

--- src/azure_function/http_context_transform.rs
+++ src/azure_function/http_context_transform.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use async_std::sync::RwLock;
-use http_types::headers::LOCATION;
 use serde_json::{json, Value};
 use tide::http::headers::CONTENT_TYPE;
 use tide::{Body, Middleware, Next, Request, Result, StatusCode};
@@ -95,6 +94,7 @@ impl AzureFnMiddleware {
         let mut res = next.run(req).await; // Continue middleware stack.
 
         let logger = Arc::try_unwrap(logger).unwrap();
+        
         let mut out = json!({
             "Outputs": {
                 "res": {
@@ -110,15 +110,22 @@ impl AzureFnMiddleware {
             "Logs": logger.into_inner().logs,
         });
 
-        if let Some(location) = res.header(LOCATION) {
-            out.pointer_mut("/Outputs/res/headers")
-                .expect("statically set json - path to headers")
-                .as_object_mut()
-                .expect("statically set json - headers as object")
-                .insert(
-                    LOCATION.as_str().to_owned(),
-                    Value::String(location.last().to_string()),
-                );
+        let headers_obj = out
+            .pointer_mut("/Outputs/res/headers")
+            .expect("statically set json - path to headers")
+            .as_object_mut()
+            .expect("statically set json - headers as object");
+        for (name, values) in res.iter() {
+            headers_obj.insert(
+                name.as_str().to_owned(),
+                Value::String(
+                    values
+                        .iter()
+                        .map(|v| v.as_str())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                ),
+            );
         }
 
         res.set_body(Body::from_json(&out)?);
